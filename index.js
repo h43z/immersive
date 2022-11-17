73cc126fd8094c5d2b81a4b4a84f52346b2ac22f
rich <h43z@users.noreply.github.com>
2022-11-17 22:11:12 +0100

    god damn

---
+++ index.js
@@ -0,0 +1,52 @@
+import Koa from 'koa'
+import Router from '@koa/router'
+import views from 'koa-views'
+import serve from 'koa-static'
+import bodyParser from 'koa-bodyparser'
+import { WebSocketServer } from 'ws'
+
+
+const app = new Koa()
+const router = new Router()
+
+router.get('/', async ctx => {
+  await ctx.render('chat.pug')
+})
+
+const wss = new WebSocketServer({ port: 8080 })
+
+wss.on('connection', ws => {
+
+  ws.color = randomColor()
+
+  ws.on('message', data =>  {
+    for(const client of wss.clients){
+      client.send(JSON.stringify({
+        msg: data+'',
+        own: client === ws, 
+        color: ws.color
+      }))
+    }
+  })
+})
+
+
+const randomColor = (_ => {
+  const randomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  }
+
+  return _ => {
+    var h = randomInt(0, 360)
+    var s = randomInt(42, 98)
+    var l = randomInt(40, 90)
+    return `hsl(${h},${s}%,${l}%)`
+  }
+})()
+
+
+app.use(bodyParser())
+app.use(serve('public'))
+app.use(views('views'))
+app.use(router.routes())
+app.listen(3034)//, '127.0.0.1')
---
+++ public/chat.js
@@ -0,0 +1,34 @@
+const ws = new WebSocket(`ws://${document.domain}:8080`)
+let lastMsgTimer = null
+
+ws.onmessage = e => {
+  const obj = JSON.parse(e.data)
+  console.log(e.data)
+  display(obj)
+}
+
+input.addEventListener('keyup', event => {
+  if(event.key === "Enter" || input.value.charCodeAt(input.value.length -1) == 32){
+    ws.send(input.value)
+    input.value = ""
+  }
+})
+
+const display = obj => {
+  clearTimeout(lastMsgTimer)
+
+  chat.style = `height: ${document.documentElement.clientHeight - input.offsetHeight - 10}px`
+
+  const s = document.createElement('span')
+  s.innerText = obj.msg
+  s.style.color = obj.own ? 'black' : obj.color
+  s.className = `msg`
+  chat.appendChild(s)
+  s.scrollIntoView()
+  
+  lastMsgTimer = setTimeout(_=>{
+    document.querySelectorAll('.msg').forEach(item => {
+      item.remove()
+    })
+  }, 3200)
+}
